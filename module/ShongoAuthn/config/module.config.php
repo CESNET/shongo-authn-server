9565c050259eb756fc74a817d4bffa1f5220ced0
Ivan Novakov <ivan.novakov@debug.cz>
2014-01-30 13:56:37 +0100

    config reorganized

--- module/ShongoAuthn/config/module.config.php
+++ module/ShongoAuthn/config/module.config.php
@@ -1,84 +1,87 @@
 <?php
 return array(
     
-    'user_factory' => array(
-        'user_class' => '\ShongoAuthn\User\User'
-    ),
-    
-    'data_connectors' => array(
-
-        'shibboleth' => array(
-            'class' => 'ShongoAuthn\User\DataConnector\Shibboleth'
+    'oic_server' => array(
+        
+        'user_factory' => array(
+            'user_class' => '\ShongoAuthn\User\User'
         ),
         
-        'perun-ws' => array(
-            'class' => 'ShongoAuthn\User\DataConnector\PerunWs',
-            'options' => array(
-                'base_url' => 'https://perun-ws.example.org/',
-                'client_id' => 'client_id',
-                'client_secret' => 'client_secret',
-                'users_resource' => 'users',
-                'principal_resource' => 'principal',
-                'http_client_config' => array(
-                    'adapter' => 'Zend\Http\Client\Adapter\Curl',
-                    'useragent' => 'Perun Client',
-                    'curloptions' => array(
+        'data_connectors' => array(
+            
+            'shibboleth' => array(
+                'class' => 'ShongoAuthn\User\DataConnector\Shibboleth'
+            ),
+            
+            'perun-ws' => array(
+                'class' => 'ShongoAuthn\User\DataConnector\PerunWs',
+                'options' => array(
+                    'base_url' => 'https://perun-ws.example.org/',
+                    'client_id' => 'client_id',
+                    'client_secret' => 'client_secret',
+                    'users_resource' => 'users',
+                    'principal_resource' => 'principal',
+                    'http_client_config' => array(
+                        'adapter' => 'Zend\Http\Client\Adapter\Curl',
+                        'useragent' => 'Perun Client',
+                        'curloptions' => array(
                         /*
                         CURLOPT_SSL_VERIFYPEER => true,
                         CURLOPT_SSL_VERIFYHOST => 2,
                         CURLOPT_CAINFO => '/etc/ssl/certs/ca-bundle.pem'
                         */
                     )
+                    )
                 )
             )
-        )
-    ),
-    
-    'user_validators' => array(
-        'perun_user' => array(
-            'class' => 'ShongoAuthn\User\Validator\PerunUser',
-            'options' => array(
-                'registration_uri' => 'https://perun.example.org/perun-registrar-fed/'
-            )
-        )
-    ),
-    
-    'user_info_mapper' => array(
-        'class' => '\ShongoAuthn\User\UserInfo\Mapper\Shongo'
-    ),
-    
-    'authentication_handlers' => array(
+        ),
         
-        'shibboleth' => array(
-            'options' => array(
-                'attribute_filter' => array(
-                    'REMOTE_USER' => array(
-                        'name' => 'eppn',
-                        'required' => true,
-                        'validators' => array()
-                    ),
-                    'mail' => array(
-                        'name' => 'mail',
-                        'required' => true,
-                        'filters' => array(
-                            'shibboleth_serialized_value' => array(
-                                'name' => 'shibboleth_serialized_value',
-                                'options' => array()
+        'user_validators' => array(
+            'perun_user' => array(
+                'class' => 'ShongoAuthn\User\Validator\PerunUser',
+                'options' => array(
+                    'registration_uri' => 'https://perun.example.org/perun-registrar-fed/'
+                )
+            )
+        ),
+        
+        'user_info_mapper' => array(
+            'class' => '\ShongoAuthn\User\UserInfo\Mapper\Shongo'
+        ),
+        
+        'authentication_handlers' => array(
+            
+            'shibboleth' => array(
+                'options' => array(
+                    'attribute_filter' => array(
+                        'REMOTE_USER' => array(
+                            'name' => 'eppn',
+                            'required' => true,
+                            'validators' => array()
+                        ),
+                        'mail' => array(
+                            'name' => 'mail',
+                            'required' => true,
+                            'filters' => array(
+                                'shibboleth_serialized_value' => array(
+                                    'name' => 'shibboleth_serialized_value',
+                                    'options' => array()
+                                )
+                            ),
+                            'validators' => array(
+                                'email' => array(
+                                    'name' => 'email_address'
+                                )
                             )
                         ),
-                        'validators' => array(
-                            'email' => array(
-                                'name' => 'email_address'
-                            )
+                        'givenName' => array(
+                            'name' => 'givenName',
+                            'required' => true
+                        ),
+                        'sn' => array(
+                            'name' => 'sn',
+                            'required' => true
                         )
-                    ),
-                    'givenName' => array(
-                        'name' => 'givenName',
-                        'required' => true
-                    ),
-                    'sn' => array(
-                        'name' => 'sn',
-                        'required' => true
                     )
                 )
             )
